d764ce2d56bf2af4eac22d7f368c8e701ab19fa9
index00913 <jisu00913@gmail.com>
2023-10-05 11:22:30 +0900

    추가5

---
+++ src/components/common/modal/Modal.jsx
@@ -0,0 +1,13 @@
+import './Modal.scss';
+
+function Modal() {
+	return (
+		<aside className='modal'>
+			Modal
+			<div className='con'></div>
+			<span>close</span>
+		</aside>
+	);
+}
+
+export default Modal;
