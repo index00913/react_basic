bcc50889eb1e952f4c12dcd75edf6787a9db7721
index00913 <jisu00913@gmail.com>
2023-10-05 16:29:17 +0900

    추가11

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -1,37 +1,51 @@
 import Layout from '../../common/layout/Layout';
 import './Contact.scss';
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 
 export default function Contact() {
 	const map = useRef(null);
-	//현재 kakao객체를 cdn으로 가져오고 있기 때문에
-	//리액트 컴포넌트안쪽에서 window객체로부터 kakao객체를 비구조화할당을 이용해서 수동으로 꺼내옴
+	const instance = useRef(null);
+	const [Traffic, setTraffic] = useState(false);
+
 	const { kakao } = window;
-	const mapOption = {
-		center: new kakao.maps.LatLng(37.58478163978524, 126.88566424098676),
-		level: 1,
+	//첫번째 지도를 출력하기 위한 객체정보
+	const info = {
+		latlng: new kakao.maps.LatLng(37.58478163978524, 126.88566424098676),
+		imgSrc: `${process.env.PUBLIC_URL}/img/marker1.png`,
+		imgSize: new kakao.maps.Size(232, 99),
+		imgPos: { offset: new kakao.maps.Point(116, 99) },
 	};
-	const imageSrc = `${process.env.PUBLIC_URL}/img/marker1.png`;
-	const imageSize = new kakao.maps.Size(232, 99);
-	const imageOption = { offset: new kakao.maps.Point(116, 99) };
 
-	const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
-
-	//마커 위치 인스턴스를 인수로 전달해서 마커 출력 인스턴스 객체를 생성
+	//위의 정보값을 활용한 마커 객체 생성
 	const marker = new kakao.maps.Marker({
-		position: mapOption.center,
-		image: markerImage,
+		position: info.latlng,
+		image: new kakao.maps.MarkerImage(info.imgSrc, info.imgSize, info.imgPos),
 	});
 
 	useEffect(() => {
-		//컴포넌트 마운트 되자마자 지도인스턴스 생성
-		const instance = new kakao.maps.Map(map.current, mapOption);
-		//마커 출력 인스턴스에 지도 인스턴스 결합
-		marker.setMap(instance);
+		//객체 정보를 활용한 지도 객체 생성
+		instance.current = new kakao.maps.Map(map.current, {
+			center: info.latlng,
+			level: 1,
+		});
+		//마커 객체에 지도 객체 연결
+		marker.setMap(instance.current);
 	}, []);
+
+	useEffect(() => {
+		//traffic 값이 바뀔때마다 실행될 구문
+		Traffic
+			? instance.current.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC)
+			: instance.current.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+	}, [Traffic]);
 
 	return (
 		<Layout title={'Contact'}>
+			{/* <button onClick={() => setTraffic(true)}>주변 교통정보 보기</button>
+			<button onClick={() => setTraffic(false)}>주변 교통정보 끄기</button> */}
+			<button onClick={() => setTraffic(!Traffic)}>
+				{Traffic ? '교통정보 끄기' : '교통정보 켜기'}
+			</button>
 			<div className='map' ref={map}></div>
 		</Layout>
 	);
