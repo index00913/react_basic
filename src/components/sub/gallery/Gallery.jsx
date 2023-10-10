4a10e71235c3b1a982d0fd061680284d0a525490
index00913 <jisu00913@gmail.com>
2023-10-10 10:30:12 +0900

    추가25

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -5,18 +5,21 @@ import Masonry from 'react-masonry-component';
 
 export default function Gallery() {
 	const [Pics, setPics] = useState([]);
-	const api_key = '2a1a0aebb34012a99c23e13b49175343';
-	const method_interest = 'flickr.interestingness.getList';
-	const num = 50;
-	const url = `https://www.flickr.com/services/rest/?method=${method_interest}&api_key=${api_key}&per_page=${num}&nojsoncallback=1&format=json`;
+
+	const fetchData = async () => {
+		const api_key = '2a1a0aebb34012a99c23e13b49175343';
+		const method_interest = 'flickr.interestingness.getList';
+		const num = 50;
+		const url = `https://www.flickr.com/services/rest/?method=${method_interest}&api_key=${api_key}&per_page=${num}&nojsoncallback=1&format=json`;
+
+		const data = await fetch(url);
+		const json = await data.json();
+		console.log(json.photos.photo);
+		setPics(json.photos.photo);
+	};
 
 	useEffect(() => {
-		fetch(url)
-			.then((data) => data.json())
-			.then((json) => {
-				console.log(json.photos.photo);
-				setPics(json.photos.photo);
-			});
+		fetchData();
 	}, []);
 
 	return (
