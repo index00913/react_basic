7794cd00e3fbf50e52c098b19b80954f26e47e3b
index00913 <jisu00913@gmail.com>
2023-10-11 10:22:07 +0900

    28추가

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -1,4 +1,5 @@
 import Layout from '../../common/layout/Layout';
+import Modal from '../../common/modal/Modal';
 import './Gallery.scss';
 import { useState, useEffect, useRef } from 'react';
 import Masonry from 'react-masonry-component';
@@ -9,8 +10,13 @@ export default function Gallery() {
 	const refBtnSet = useRef(null);
 	const [Pics, setPics] = useState([]);
 	const [Loader, setLoader] = useState(true);
+	const [ActiveURL, setActiveURL] = useState('');
+	const [Fix, setFix] = useState(false);
+	const [IsUser, setIsUser] = useState(true);
+	const [Open, setOpen] = useState(false);
 	const my_id = '164021883@N04';
 
+	//처음 마운트 데이터 호출 함수
 	const fetchData = async (opt) => {
 		let count = 0;
 		setLoader(true);
@@ -20,10 +26,8 @@ export default function Gallery() {
 		const method_interest = 'flickr.interestingness.getList';
 		const method_user = 'flickr.people.getPhotos';
 		const method_search = 'flickr.photos.search';
-		const num = 10;
+		const num = 50;
 
-		//fetching함수 호출시 타입값이 있는 객체를 인수로 전달하면 해당 타입에 따라 호출 URL이 변경되고
-		//해당URL을 통해 받아지는 데이터로 달라짐
 		if (opt.type === 'interest') {
 			url = `https://www.flickr.com/services/rest/?method=${method_interest}&api_key=${api_key}&per_page=${num}&nojsoncallback=1&format=json`;
 		}
@@ -40,21 +44,14 @@ export default function Gallery() {
 		if (json.photos.photo.length === 0) {
 			return alert('해당 검색어의 결과값이 없습니다.');
 		}
-		//실제 데이터가 state에 담기는 순간 가상돔이 생성되는 순간
 		setPics(json.photos.photo);
 
 		const imgs = refFrame.current?.querySelectorAll('img');
-		console.log(imgs);
 
 		imgs.forEach((img) => {
 			img.onload = () => {
 				++count;
-				console.log('현재 로딩된 img갯수', count);
-				//interest gallery에서 특정 사용자 갤러리 호출시 이미 interest화면에서 2개의 이미지 이미 캐싱처리 되어 있기 때문에
-				//전체 이미지 갯수에서 -2를 빼줘야지 무한로딩 오류 해결
-				if (count === imgs.length - 2) {
-					console.log('모든 이미지 소스 렌더링 완료!');
-					//모든 소스이미지라 렌더링완료되면 Loader값을 false로 바꿔서 로딩이미지 제거
+				if (count === (Fix ? imgs.length / 2 - 1 : imgs.length - 2)) {
 					setLoader(false);
 					refFrame.current.classList.add('on');
 				}
@@ -62,107 +59,131 @@ export default function Gallery() {
 		});
 	};
 
+	//submit이벤트 발생시 실행할 함수
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setIsUser(false);
+
+		const btns = refBtnSet.current.querySelectorAll('button');
+		btns.forEach((btn) => btn.classList.remove('on'));
+
+		if (refInput.current.value.trim() === '') {
+			return alert('검색어를 입력하세요.');
+		}
+
+		fetchData({ type: 'search', tags: refInput.current.value });
+		refInput.current.value = '';
+	};
+
+	//myGallery 클릭 이벤트 발생시 실행할 함수
+	const handleClickMy = (e) => {
+		setIsUser(true);
+		if (e.target.classList.contains('on')) return;
+
+		const btns = refBtnSet.current.querySelectorAll('button');
+		btns.forEach((btn) => btn.classList.remove('on'));
+		e.target.classList.add('on');
+
+		fetchData({ type: 'user', id: my_id });
+	};
+
+	//Interest Gallery 클릭 이벤트 발생시 실행할 함수
+	const handleClickInterest = (e) => {
+		setIsUser(false);
+		if (e.target.classList.contains('on')) return;
+
+		const btns = refBtnSet.current.querySelectorAll('button');
+		btns.forEach((btn) => btn.classList.remove('on'));
+		e.target.classList.add('on');
+
+		fetchData({ type: 'interest' });
+	};
+
+	//profile 아이디 클릭시 실행할 함수
+	const handleClickProfile = (e) => {
+		if (IsUser) return;
+		fetchData({ type: 'user', id: e.target.innerText });
+		setIsUser(true);
+	};
+
 	useEffect(() => {
 		fetchData({ type: 'user', id: my_id });
 	}, []);
 
 	return (
-		<Layout title={'Gallery'}>
-			<div className='searchBox'>
-				<form
-					onSubmit={(e) => {
-						e.preventDefault();
-						if (refInput.current.value.trim() === '') {
-							return alert('검색어를 입력하세요.');
-						}
+		<>
+			<Layout title={'Gallery'}>
+				<div className='searchBox'>
+					<form onSubmit={handleSubmit}>
+						<input ref={refInput} type='text' placeholder='검색어를 입력하세요' />
+						<button>검색</button>
+					</form>
+				</div>
 
-						fetchData({ type: 'search', tags: refInput.current.value });
-						refInput.current.value = '';
-					}}
-				>
-					<input ref={refInput} type='text' placeholder='검색어를 입력하세요' />
-					<button>검색</button>
-				</form>
-			</div>
+				<div className='btnSet' ref={refBtnSet}>
+					<button className='on' onClick={handleClickMy}>
+						My Gallery
+					</button>
 
-			<div className='btnSet' ref={refBtnSet}>
-				<button
-					className='on'
-					onClick={(e) => {
-						//각 버튼 클릭시 해당 버튼에 만약 on클래스가 있으면 이미 활성화 되어 있는 버튼이므로 return으로 종료해서
-						//fetchData함수 호출 방지
-						if (e.target.classList.contains('on')) return;
+					<button onClick={handleClickInterest}>Interest Gallery</button>
+				</div>
 
-						//클릭한 버튼요소에 on이없으면 해당 버튼활성화
-						const btns = refBtnSet.current.querySelectorAll('button');
-						btns.forEach((btn) => btn.classList.remove('on'));
-						e.target.classList.add('on');
-
-						fetchData({ type: 'user', id: my_id });
-					}}
-				>
-					My Gallery
-				</button>
-				<button
-					onClick={(e) => {
-						//각 버튼 클릭시 해당 버튼에 만약 on클래스가 있으면 이미 활성화 되어 있는 버튼이므로 return으로 종료해서
-						//fetchData함수 호출 방지
-						if (e.target.classList.contains('on')) return;
-
-						//클릭한 버튼요소에 on이없으면 해당 버튼활성화
-						const btns = refBtnSet.current.querySelectorAll('button');
-						btns.forEach((btn) => btn.classList.remove('on'));
-						e.target.classList.add('on');
-
-						fetchData({ type: 'interest' });
-					}}
-				>
-					Interest Gallery
-				</button>
-			</div>
-
-			{/* Loader가 true일때에만 로딩 이미지 출력 */}
-			{Loader && (
-				<img className='loading' src={`${process.env.PUBLIC_URL}/img/loading.gif`} alt='loading' />
-			)}
-			<div className='picFrame' ref={refFrame}>
-				<Masonry
-					elementType={'div'}
-					options={{ transitionDuration: '0.5s' }}
-					disableImagesLoaded={false}
-					updateOnEachImageLoad={false}
-				>
-					{Pics.map((data, idx) => {
-						return (
-							<article key={idx}>
-								<div className='inner'>
-									<img
-										className='pic'
-										src={`https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_m.jpg`}
-										alt={`https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_b.jpg`}
-									/>
-									<h2>{data.title}</h2>
-
-									<div className='profile'>
+				{Loader && (
+					<img
+						className='loading'
+						src={`${process.env.PUBLIC_URL}/img/loading.gif`}
+						alt='loading'
+					/>
+				)}
+				<div className='picFrame' ref={refFrame}>
+					<Masonry
+						elementType={'div'}
+						options={{ transitionDuration: '0.5s' }}
+						disableImagesLoaded={false}
+						updateOnEachImageLoad={false}
+					>
+						{Pics.map((data, idx) => {
+							return (
+								<article key={idx}>
+									<div className='inner'>
 										<img
-											src={`http://farm${data.farm}.staticflickr.com/${data.server}/buddyicons/${data.owner}.jpg`}
-											alt={data.owner}
-											onError={(e) => {
-												//만약 사용자가 프로필 이미지를 올리지 않았을때 엑박이 뜨므로
-												//onError이벤트를 연결해서 대체이미지 출력
-												e.target.setAttribute('src', 'https://www.flickr.com/images/buddyicon.gif');
+											className='pic'
+											src={`https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_m.jpg`}
+											alt={`https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_b.jpg`}
+											onClick={(e) => {
+												setActiveURL(e.target.getAttribute('alt'));
+												setOpen(true);
 											}}
 										/>
-										<span onClick={() => fetchData({ type: 'user', id: data.owner })}>
-											{data.owner}
-										</span>
+										<h2>{data.title}</h2>
+
+										<div className='profile'>
+											<img
+												src={`http://farm${data.farm}.staticflickr.com/${data.server}/buddyicons/${data.owner}.jpg`}
+												alt={data.owner}
+												onError={(e) => {
+													setFix(true);
+													e.target.setAttribute(
+														'src',
+														'https://www.flickr.com/images/buddyicon.gif'
+													);
+												}}
+											/>
+											<span onClick={handleClickProfile}>{data.owner}</span>
+										</div>
 									</div>
-								</div>
-							</article>
-						);
-					})}
-				</Masonry>
-			</div>
-		</Layout>
+								</article>
+							);
+						})}
+					</Masonry>
+				</div>
+			</Layout>
+
+			{Open && (
+				<Modal>
+					<img src={ActiveURL} alt='img' />
+				</Modal>
+			)}
+		</>
 	);
 }
