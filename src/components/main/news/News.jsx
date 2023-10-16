b8278eefc303d605bdc34ceda7b62887c33baed0
index00913 <jisu00913@gmail.com>
2023-10-16 15:30:16 +0900

    스타일 수정

--- src/components/main/news/News.jsx
+++ src/components/main/news/News.jsx
@@ -2,30 +2,28 @@ import './News.scss';
 import { useState } from 'react';
 
 function News() {
-	//프로젝트가 처음 구동되면 무조건 메인페이지에 갔다가 커뮤니티 페이지에 넘어가는 구조
-	//해당 페이지에 있는 함수가 처음 구동될시 로컬저장소에 값이 없으므로
-	//저장소값이 없을때 빈배열이 반환되는 구문을 추가 (중요)
 	const getLocalData = () => {
 		const data = localStorage.getItem('post');
-		if (data) return JSON.parse(data);
-		else return [];
+
+		return JSON.parse(data);
 	};
 	const [Post] = useState(getLocalData());
 
 	return (
 		<section className='news'>
 			<h2>News</h2>
-			<div className='postWrap'>
-				{Post.map((el, idx) => {
-					if (idx >= 4) return null;
-					else
-						return (
-							<article key={idx}>
-								<h2>{el.title}</h2>
-								<p>{el.content}</p>
-							</article>
-						);
-				})}
+			<div className='box1'>
+				<img src='/img/code1.jpg' alt='' />
+				<img src='/img/code2.jpg' alt='' />
+				<img src='/img/code3.jpeg' alt='' />
+				<img src='/img/code4.jpeg' alt='' />
+				<p className='text1'>1ST FW released</p>
+				<p className='text2'>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum blanditiis explicabo tempore
+					modi magni saepe, tempora iste. Consequuntur explicabo, accusamus, reprehenderit, sequi
+					eveniet ab optio iste dolore molestiae aliquam ducimus Lorem ipsum dolor sit amet
+					consectetur adipisicing elit. Esse, repudiandae.
+				</p>
 			</div>
 		</section>
 	);
--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -4,6 +4,8 @@ function Visual() {
 	return (
 		<section className='visual'>
 			<h2>Visual</h2>
+			<img src='/img/img1.jpg' alt='' />
+			<p className='text1'>Visual</p>
 		</section>
 	);
 }
