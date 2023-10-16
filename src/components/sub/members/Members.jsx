160a1dd9409dff4e95f769fa05274c1dfafdc19e
index00913 <jisu00913@gmail.com>
2023-10-16 10:46:54 +0900

    인증에러 전체 흐름

--- src/components/sub/members/Members.jsx
+++ src/components/sub/members/Members.jsx
@@ -1,12 +1,37 @@
 import Layout from '../../common/layout/Layout';
 import './Members.scss';
+import { useState } from 'react';
 
 export default function Members() {
+	const initVal = {
+		userid: '',
+		pwd1: '',
+		pwd2: '',
+		email: '',
+	};
+	const [Val, setVal] = useState(initVal);
+
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setVal({ ...Val, [name]: value });
+	};
+
+	//인수값으로 state를 전달받아서 각 데이터별로 인증처리후
+	//만약 인증에러가 발생하면 해당 name값으로 에러문구를 생성해서 반환하는 함수
+	const check = (value) => {};
+
+	//전송이벤트 발생시 state에 있는 인풋값들을 check함수에 전달해서 호출
+	//만약 check함수가 에러객체를 하나도 내보내지 않으면 인증성공
+	//하나라도 에러객체가 전달되면 인증실패처리하면서 name값과 매칭이 되는 input요소 아래쪽에 에러메세지 출력
+	const handleSubmit = (e) => {
+		e.preventDefault();
+	};
+
 	return (
 		<Layout title={'Members'}>
-			<form>
+			<form onSubmit={handleSubmit}>
 				<fieldset>
-					<legend>회원가입 폼 양식</legend>
+					<legend className='h'>회원가입 폼 양식</legend>
 					<table border='1'>
 						<tbody>
 							{/* userid */}
@@ -15,7 +40,13 @@ export default function Members() {
 									<label htmlFor='userid'>userid</label>
 								</th>
 								<td>
-									<input type='text' id='userid' name='userid' />
+									<input
+										type='text'
+										id='userid'
+										name='userid'
+										value={Val.userid}
+										onChange={handleChange}
+									/>
 								</td>
 							</tr>
 
@@ -25,7 +56,13 @@ export default function Members() {
 									<label htmlFor='pwd1'>password</label>
 								</th>
 								<td>
-									<input type='password' id='pwd1' name='pwd1' />
+									<input
+										type='password'
+										id='pwd1'
+										name='pwd1'
+										value={Val.pwd1}
+										onChange={handleChange}
+									/>
 								</td>
 							</tr>
 
@@ -35,8 +72,38 @@ export default function Members() {
 									<label htmlFor='pwd2'>re-password</label>
 								</th>
 								<td>
-									<input type='password' id='pwd2' name='pwd2' />
+									<input
+										type='password'
+										id='pwd2'
+										name='pwd2'
+										value={Val.pwd2}
+										onChange={handleChange}
+									/>
 								</td>
+							</tr>
+
+							{/* email */}
+							<tr>
+								<th scope='row'>
+									<label htmlFor='email'>e-mail</label>
+								</th>
+								<td>
+									<input
+										type='text'
+										id='email'
+										name='email'
+										value={Val.email}
+										onChange={handleChange}
+									/>
+								</td>
+							</tr>
+
+							{/* btnSet */}
+							<tr>
+								<th colSpan='2'>
+									<input type='reset' value='cancel' />
+									<input type='submit' value='send' />
+								</th>
 							</tr>
 						</tbody>
 					</table>
