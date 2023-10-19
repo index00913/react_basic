6d0338a79bed01b34a5201549fd140ea8ee17da7
index00913 <jisu00913@gmail.com>
2023-10-19 15:38:24 +0900

    버튼 수정

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -2,7 +2,7 @@ import './Visual.scss';
 
 function Visual() {
 	return (
-		<section className='visual myScroll'>
+		<section className='visual'>
 			<h2>Visual</h2>
 			<img src='/img/img1.jpg' alt='' />
 			<p className='text1'>Visual</p>
